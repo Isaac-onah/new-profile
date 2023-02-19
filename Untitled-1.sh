be10ae7b0dcec4a3e62917fb093c5937e4b24f70
Isaac Onah <iumeha@yahoo.com>
2023-02-19 11:42:45 +0100

    commit message

---
+++ Untitled-1.sh
@@ -0,0 +1,104 @@
+
+git commit --date='2023-02-13 10:12:45' -m 'commit message' - done
+git commit --date='2023-02-13 12:12:45' -m 'commit message'
+
+git commit --date='2023-02-14 10:12:45' -m 'commit message'
+git commit --date='2023-02-14 10:30:45' -m 'commit message' - done
+git commit --date='2023-02-14 11:15:45' -m 'commit message'-done
+git commit --date='2023-02-14 11:22:45' -m 'commit message'
+git commit --date='2023-02-14 12:12:45' -m 'commit message' - done
+
+git commit --date='2023-02-15 11:45:45' -m 'commit message'
+
+git commit --date='2023-02-16 11:45:45' -m 'commit message'
+git commit --date='2023-02-16 12:12:45' -m 'commit message'
+
+git commit --date='2023-02-17 10:12:45' -m 'commit message'
+git commit --date='2023-02-17 10:30:45' -m 'commit message'
+git commit --date='2023-02-17 11:15:45' -m 'commit message'
+git commit --date='2023-02-17 11:22:45' -m 'commit message'
+git commit --date='2023-02-17 12:12:45' -m 'commit message'
+
+git commit --date='2023-02-18 12:12:45' -m 'commit message'
+
+git commit --date='2023-02-19 11:11:45' -m 'commit message'
+git commit --date='2023-02-19 11:13:45' -m 'commit message'
+git commit --date='2023-02-19 11:17:45' -m 'commit message'
+git commit --date='2023-02-19 11:22:45' -m 'commit message'
+git commit --date='2023-02-19 11:32:45' -m 'commit message' - done
+git commit --date='2023-02-19 11:42:45' -m 'commit message'
+git commit --date='2023-02-19 12:42:45' -m 'commit message'
+
+git commit --date='2023-02-20 12:42:45' -m 'commit message'
+
+
+git commit --date='2023-02-21 12:12:45' -m 'commit message'
+git commit --date='2023-02-21 12:22:45' -m 'commit message'
+git commit --date='2023-02-21 12:32:45' -m 'commit message'
+git commit --date='2023-02-21 12:42:45' -m 'commit message'
+
+git commit --date='2023-02-22 12:12:45' -m 'commit message'
+git commit --date='2023-02-22 12:22:45' -m 'commit message'
+
+git commit --date='2023-02-23 12:12:45' -m 'commit message'
+git commit --date='2023-02-23 12:22:45' -m 'commit message'
+
+git commit --date='2023-02-24 12:12:45' -m 'commit message'
+
+git commit --date='2023-02-25 12:12:45' -m 'commit message'
+git commit --date='2023-02-25 12:22:45' -m 'commit message'
+
+git commit --date='2023-02-26 11:11:45' -m 'commit message'
+git commit --date='2023-02-26 11:13:45' -m 'commit message'
+git commit --date='2023-02-26 11:17:45' -m 'commit message'
+git commit --date='2023-02-26 11:22:45' -m 'commit message'
+git commit --date='2023-02-26 11:32:45' -m 'commit message'
+git commit --date='2023-02-26 11:42:45' -m 'commit message'
+git commit --date='2023-02-26 12:42:45' -m 'commit message'
+
+git commit --date='2023-02-27 12:42:45' -m 'commit message'
+
+
+git commit --date='2023-02-28 12:12:45' -m 'commit message'
+git commit --date='2023-02-28 12:22:45' -m 'commit message'
+git commit --date='2023-02-28 12:32:45' -m 'commit message'
+git commit --date='2023-02-28 12:42:45' -m 'commit message'
+
+git commit --date='2023-03-02 12:12:45' -m 'commit message'
+git commit --date='2023-03-02 12:22:45' -m 'commit message'
+
+git commit --date='2023-03-03 12:12:45' -m 'commit message'
+git commit --date='2023-03-03 12:22:45' -m 'commit message'
+
+git commit --date='2023-03-04 12:12:45' -m 'commit message'
+
+git commit --date='2023-03-05 12:12:45' -m 'commit message'
+git commit --date='2023-03-05 12:22:45' -m 'commit message'
+
+.services__modal-content{
+  position: relative;
+  background-color: var(--body-color);
+  padding: 4.5rem 1.5rem 2.5rem;
+  border-radius: 1.5rem;
+}
+
+.services__modal-title,
+.services__modal-description{
+  text-align: center;
+}
+
+.services__modal-title{
+  font-size: var(--h3-font-size);
+  color: var(--first-color);
+  margin-bottom: 1rem;
+}
+
+.services__modal-description{
+  font-size: var(--small-font-size);
+  margin-bottom: 2rem;
+}
+
+.services__modal-list{
+  display: grid;
+  row-gap: .75rem;
+}
