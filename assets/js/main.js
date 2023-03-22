df2e5e080960be922b79dfd5b4ccb6127ccd24fa
Isaac Onah <iumeha@yahoo.com>
2023-03-22 12:12:45 +0100

    commit message

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,27 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
 /*=============== LIGHT DARK THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//previously selected topic (if user selection)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//we obtaine the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+
+//we validate if the user previously chose a topic
+
+if (selectedTheme){
+    //if the validation is fulfilled, we ask waht the issue was to know is we activated or deactivated the dark
+    
+}
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
