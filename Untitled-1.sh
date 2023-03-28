ef72f3de6eec7d0f18ac88ff34656e1bdc3a001a
Isaac Onah <iumeha@yahoo.com>
2023-03-28 12:12:45 +0100

    commit message

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,12 +1,4 @@
 
-
-git commit --date='2023-03-27 11:11:45' -m 'commit message'
-git commit --date='2023-03-27 11:13:45' -m 'commit message'
-git commit --date='2023-03-27 11:17:45' -m 'commit message'
-git commit --date='2023-03-27 11:22:45' -m 'commit message'
-git commit --date='2023-03-27 11:32:45' -m 'commit message'
-git commit --date='2023-03-27 11:42:45' -m 'commit message'
-
 git commit --date='2023-03-28 12:12:45' -m 'commit message'
 git commit --date='2023-03-28 12:22:45' -m 'commit message'
 git commit --date='2023-03-28 12:32:45' -m 'commit message'
