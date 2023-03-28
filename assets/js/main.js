6333c422e9fddeddcb980f1f813180e7ade35645
Isaac Onah <iumeha@yahoo.com>
2023-03-28 12:32:45 +0100

    commit message

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,5 +129,8 @@ themeButton.addEventListener('click', ()=>{
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 const sr = ScrollReveal({
-    origin: 'top'
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
 })
